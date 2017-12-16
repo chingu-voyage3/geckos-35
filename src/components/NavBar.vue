d958a78be8ece2a7bf220b7ba931bf8eb6ab2039
Ege <esekercioglu@gmail.com>
2017-12-17 02:07:09 +0300

    initial prototype commit

---
+++ src/components/NavBar.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="sidebar-container">
+    <span>SideBar</span>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style lang="scss"scoped>
+.sidebar-container {
+  width:220px;
+  height:100%;
+  background-color: red;
+  background-color: rgba(0,0,0,.5);
+
+}
+</style>
